a9d15eeebf2470404e1f7535a56ce1e3152ebde0
Kevin Zhang <kevinctzhang@gmail.com>
2016-08-04 22:22:45 -0700

    working on iterSolver

--- OptimialGreedySolutionMinimizeInjectivePairing/main.cpp
+++ OptimialGreedySolutionMinimizeInjectivePairing/main.cpp
@@ -15,25 +15,41 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    bool IterMode = true, RecMode = true;
-    if (argc != 2) {
-        cout << "Recursive Usage: \"Match R < sample.in\"" << endl;
-        cout << "Iterative Usage: \"Match I < sample.in\"" << endl;
-        return 0;
-    }
+    bool IterMode = true, RecMode = false;
+//    if (argc != 2) {
+//        cout << "Recursive Usage: \"Match R < sample.in\"" << endl;
+//        cout << "Iterative Usage: \"Match I < sample.in\"" << endl;
+//        return 0;
+//    }
     
-    if (argv[1][0] == 'I') {
-        RecMode = false;
-    } else if (argv[1][0] == 'R') {
-        IterMode = false;
-    } else {
-        cout << "Recursive Usage: \"Match R < sample.in\"" << endl;
-        cout << "Iterative Usage: \"Match I < sample.in\"" << endl;
-        return 0;
-    }
+//    if (argv[1][0] == 'I') {
+//        RecMode = false;
+//    } else if (argv[1][0] == 'R') {
+//        IterMode = false;
+//    } else {
+//        cout << "Recursive Usage: \"Match R < sample.in\"" << endl;
+//        cout << "Iterative Usage: \"Match I < sample.in\"" << endl;
+//        return 0;
+//    }
     
     ProblemInfo *PInfo = new ProblemInfo();
-    PInfo->Read();
+    PInfo->m = 5;
+    PInfo->n = 5;
+    PInfo->S = new int[PInfo->m];
+    PInfo->H = new int[PInfo->n];
+    for (int i = 160; i <= 160+PInfo->m; ++i) {
+        PInfo->S[i-160]= i;
+        cout << " " << PInfo->S[i-160];
+    }
+    cout << " " << endl;
+    for (int i = 170; i <= 170+PInfo->n; ++i) {
+        PInfo->H[i-170]= i;
+        cout << " " << PInfo->H[i-170];
+    }
+    
+
+    
+//    PInfo->Read();
     
     if (IterMode) {
         cout << " " << IterSolve(PInfo) << endl;
